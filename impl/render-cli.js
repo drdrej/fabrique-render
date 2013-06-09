0f636763db371bb9f529b643b228f3daf16fb17b
Andreas Siebert <andrej.siebert@gmail.com>
2013-06-09 23:00:19 +0200

    working on cli

--- impl/render-cli.js
+++ impl/render-cli.js
@@ -31,11 +31,48 @@
  *
  * render file1 ... file3 from directory passed in "-src" parameter to directory passed in
  * "-dest" parameter.
+ *
  */
+
+ /** context to store parameter to call a render-command with
+ * resolved pathes.
+ *
+ * @type {{object}}
+ */
+var params = {};
+
+
+
+var _ = require( "underscore" );
+var path = require( "path" );
+
+/**
+ * function to resolve path of a template.
+ *
+ * @param dir
+ * @param file
+ */
+function resolvePath( dir, file ) {
+    var root = dir;
+    if( !_.isString(dir) ) {
+        LOGGER.warn( "can't resolve a path, because passed parameter:dir is not a string. dir = " + dir );
+        return false;
+    }
+
+    if( !_.isString(file) ) {
+        LOGGER.warn( "can't resolve a path, because passed parameter:file is not a string. file = " + file );
+        return false;
+    }
+
+    var full = dir + "/" + file;
+    var normalized = path.normalize( full);
+
+    return path.resolve( normalized );
+};
 
 
 var optimist = require('optimist')
-    .usage( '$0 -model ./my-data.json -src "..." -dest "..." file1.tmpl, file2.tmpl, file3.tmpl' )
+    .usage( '$0 --model ./my-data.json --src "..." --dest "..." file1.tmpl, file2.tmpl, file3.tmpl' )
     .demand( [ 'm', 's', 'd' ] )
 
     .alias( 'm', 'model')
@@ -47,36 +84,53 @@ var optimist = require('optimist')
     .alias( 'd', 'dest' )
     .describe( 'd', 'destination-directory where rendered files should be stored' )
 
-    .check( function(argv) {
-            if ( !argv._.length ) {
-                throw 'Must define at least one template-file.';
+    .check( function( argv ) {
+        if ( !argv._.length ) {
+            throw 'Must define at least one template-file.';
+        }
+
+        argv._.forEach( function( tmplName ) {
+            var tmplPath = resolvePath( argv.s, tmplName );
+
+            try {
+                fs.statSync( tmplPath );
+                params.templates.push( tmplPath );
+            } catch (err) {
+                throw 'Unable to open template file "' + tmplPath + '"';
             }
-
-            argv._.forEach( function(template) {
-                try {
-                    console.log( '-- consoleeee: ' + template  );
-                    fs.statSync(template);
-                } catch (err) {
-                    throw 'Unable to open template file "' + template + '"';
-                }
-            });
-    })
-
-    ;
-
+        });
+    });
 
 
 var args = optimist.argv;
 
 var render = require( "./render-impl.js" );
 
+/*
+.check( function(argv) {
+    if ( !argv._.length ) {
+        throw 'Must define at least one template-file.';
+    }
+
+    argv._.forEach( function(template) {
+        try {
+            fs.statSync(template);
+        } catch (err) {
+            throw 'Unable to open template file "' + template + '"';
+        }
+    });
+});
+*/
+
+
 args._.forEach( function( template ) {
-    // try {
+    try {
         // param: template - must be a path to a template file.
-        render( template, args.m );
-    // } catch (err) {
-    //    throw 'Unable to open template file "' + template + '"';
-    // }
+        var rendered = render( template, args.m );
+        console.log( ">>>> rendered: " + rendered );
+    } catch (err) {
+        LOGGER.warn( 'Unable to render template: "' + template + '". skip it!' );
+    }
 });
 
 
